dd09935780536d800ea791b99010b9b9ead247e3
aeshab <46256704+aeshab@users.noreply.github.com>
2019-01-17 09:01:42 +0530

    Add files via upload

---
+++ pattern2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p_1
+{
+    class pattern2
+    {
+        public static void Main()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
