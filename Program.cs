9d1d1bcdf2bb03615755b7ef388c756141da610b
aeshab <46256704+aeshab@users.noreply.github.com>
2019-02-07 08:45:51 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,25 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
-namespace p_1
+namespace Refelction
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
-        {
-            for(int i=5;i>0;i--)
-            {
-                for(int j=i;j>0;j--)
-                {
-                    Console.Write("@");
 
-                }
-                Console.WriteLine();
+        static void Main()
+        { 
+            Type T = Type.GetType("Refelction.Student");
+            MethodInfo[] methods = T.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType+" "+method.Name);
+            
             }
 
+            PropertyInfo[] properties = T.GetProperties();
+            
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.PropertyType+" "+ property.Name);        
+            }
+
+            Console.WriteLine("\nConstructors");
+            ConstructorInfo[] constructors = T.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
             Console.ReadKey();
         }
+    }
+
+   
+    
+
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Student(int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+        public Student()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+        
+        }
+
+        public void printId()
+        {
+            Console.WriteLine("ID = {0}", this.Id);
+
+        }
+        public void printName()
+        {
+            Console.WriteLine("Name={0}",this.Name);
+        }
+
     }
 }
