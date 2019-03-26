f1d9a2021030c48737fcda5afd39c501861216c3
aeshab <46256704+aeshab@users.noreply.github.com>
2019-03-27 00:12:59 +0530

    Create copyfile.cs

---
+++ copyfile.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+
+public class CopyFile
+{
+    public void copyFile(string file1, string file2)
+    { 
+        using(StreamReader reader = new StreamReader(file1))
+        using (StreamWriter writer = new StreamWriter(file2))
+        {
+            string line = null;
+            while ((line = reader.ReadLine()) != null)
+                writer.WriteLine(line);
+        }
+    }
+}
+class MainClass
+{
+    public static void Main()
+    {
+        CopyFile cp = new CopyFile();
+        string file1 = @"C:\Users\AnjaliBalchandani\File\1.txt";
+        string file2 = @"C:\Users\AnjaliBalchandani\File\2.txt";
+        cp.copyFile(file1,file2);
+    }
+}
